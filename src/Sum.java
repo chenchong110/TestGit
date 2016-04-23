e24889cb80b01ee39f69af6d4f93cdd9311f7894
chenchong <280207685@qq.com>
2016-04-23 15:51:34 +0800

    First commit

---
+++ src/Sum.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Sum {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int i;
+		String s1,s2;
+		InputStreamReader isr=new InputStreamReader(System.in);
+		BufferedReader buf=new BufferedReader(isr);
+		s1=buf.readLine();
+		i=Integer.parseInt(s1);
+		i+=1;
+		System.out.println(i);
+	}
+
+}
+
