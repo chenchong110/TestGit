1fe59e113b8793c9cc50e6c4f18dac3778e0e7ea
chenchong <280207685@qq.com>
2016-04-23 16:18:09 +0800

    改改

--- src/Sum.java
+++ src/Sum.java
@@ -8,7 +8,7 @@ public class Sum {
 	/**
 	 * @param args
 	 * @throws IOException 
-	 * master
+	 * master1
 	 */
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
